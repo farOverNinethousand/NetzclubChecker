ee1f3b42e2373010d8bcdaa1757999b628e3fc51
Markus Wernersbach <m.wernersbach@ppc-ag.de>
2020-02-07 12:26:42 +0100

    fixed log file path

--- automate.py
+++ automate.py
@@ -4,18 +4,22 @@ import subprocess
 import time
 import random
 import os
+import datetime
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 CHECKER_PATH = "./Checker.py"
 CHECKER_PATH_ABS = os.path.join(DIR_PATH, CHECKER_PATH)
 
+LOG_PATH = "./status.log"
+LOG_PATH_ABS = os.path.join(DIR_PATH, LOG_PATH)
+
 while True: 
 	cmd = ['python3', 'CHECKER_PATH_ABS'] 
 	subprocess.Popen(cmd).wait()
 
-	with open("/home/pi/NetzclubChecker/status.log", "w") as f:
-		f.write("Programm ausgefuehrt")
+	with open(LOG_PATH_ABS, "w") as f:
+		f.write("Programm ausgefuehrt zuletzt am {}".format(datetime.datetime.now()))
 	waittime = random.randint(10, 36000)
 	print("Wait for {} seconds ({} hours)".format(waittime, waittime/60/60))
 	time.sleep(waittime)
